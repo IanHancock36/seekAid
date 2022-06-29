98a5e3aab3925650e9d2f1a97ee9cce9ad8ace8d
IanHancock36 <northroadian@gmail.com>
2022-06-29 07:03:45 -0500

    firebase config

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,17 +1,30 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {initializeApp} from 'firebase/app';
+import {getAuth, createUserWithEmailAndPassword} from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDrdmp5qdMBeS_VE8J5s0DvrdQPDpp6POc",
-  authDomain: "seek-to-do-more-f543b.firebaseapp.com",
-  projectId: "seek-to-do-more-f543b",
-  storageBucket: "seek-to-do-more-f543b.appspot.com",
-  messagingSenderId: "103358106731",
-  appId: "1:103358106731:web:6c57cb976b23671404f52e"
+  apiKey: 'AIzaSyDrdmp5qdMBeS_VE8J5s0DvrdQPDpp6POc',
+  authDomain: 'seek-to-do-more-f543b.firebaseapp.com',
+  projectId: 'seek-to-do-more-f543b',
+  storageBucket: 'seek-to-do-more-f543b.appspot.com',
+  messagingSenderId: '103358106731',
+  appId: '1:103358106731:web:6c57cb976b23671404f52e',
 };
+
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
