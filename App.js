9649f3314be0f69414bf9d674cfff51b8b943694
IanHancock36 <northroadian@gmail.com>
2022-07-25 05:42:56 -0500

    nav;

--- App.js
+++ App.js
@@ -57,7 +57,7 @@ const App = () => {
     <NativeBaseProvider>
       <NavigationContainer>
         {/* <DailyCheckList /> */}
-        {/* <SignIn /> */}
+        {/* <SignUp/> */}
         <ChallengeFaq />
       </NavigationContainer>
     </NativeBaseProvider>
