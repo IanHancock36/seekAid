6f571b08efd5c5ffe369913a68779dd51cf4c33a
IanHancock36 <northroadian@gmail.com>
2022-07-30 06:47:23 -0500

    firebase auth

--- firebase.js
+++ firebase.js
@@ -14,4 +14,11 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+const auth = firebase.auth();
+export {auth};
