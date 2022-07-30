a7eba5ed03c6a06b6a9d491fe5ae07f5301389b8
IanHancock36 <northroadian@gmail.com>
2022-07-30 06:43:51 -0500

    firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from 'firebase';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyD3foKA1fj2PpP6h0sAhl2QEv0WUdSF2A4',
+  authDomain: 'seek-aid-3eea7.firebaseapp.com',
+  projectId: 'seek-aid-3eea7',
+  storageBucket: 'seek-aid-3eea7.appspot.com',
+  messagingSenderId: '52156152234',
+  appId: '1:52156152234:web:22ea053d2df39eeb91d3d3',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
